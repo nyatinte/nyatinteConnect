3592999de17b7c901470372994ea24dbd8e21f3c
nyatinte <takumi.062757@gmail.com>
2023-11-15 22:35:44 +0900

    🚚AvatarをSkeletonUIに置き換え

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import Avatar from '$lib/components/UI/Avatar/Avatar.svelte';
+</script>
+
+<Avatar src="/nyatinte.png" />
 <h1>Nyatinte Connect</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
