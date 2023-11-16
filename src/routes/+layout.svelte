436b28209eedfcce52a0628081de05a22f61231a
nyatinte <takumi.062757@gmail.com>
2023-11-16 20:13:21 +0900

    💄背景色のデザインを修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,6 @@
 	<meta name="description" content="Nyatinteの全てがここに" />
 </svelte:head>
 
-<div class="h-screen w-screen bg-gradient-to-b from-green-100 via-teal-300 to-blue-300">
+<div class="h-screen w-screen bg-gradient-to-b from-green-100/50 via-teal-300/50 to-blue-300/50">
 	<slot />
 </div>
