7bf017ca5797622e17fb4443ec36fced75fd196d
nyatinte <takumi.062757@gmail.com>
2023-11-18 15:42:55 +0900

    feat: ホーム画面のワイヤーを作製

---
+++ src/lib/components/UI/LinkCard/LinkCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string;
+	export let href: string;
+	export let title: string;
+	export let description: string;
+</script>
+
+<a
+	class="card card-hover flex h-24 w-full items-center gap-4 overflow-hidden rounded-lg bg-white/60 p-4"
+	{href}
+>
+	<img {src} class="aspect-square w-14 rounded-full bg-black/50 shadow-md" {alt} />
+	<dl class="flex-1">
+		<dt class="text-lg">{title}</dt>
+		<dd class="text-sm text-gray-800">{description}</dd>
+	</dl>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import Avatar from '$lib/components/UI/Avatar/Avatar.svelte';
+	import LinkCard from '$lib/components/UI/LinkCard/LinkCard.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const { linkCards } = data;
 </script>
 
-<Avatar src="/nyatinte.png" />
-<h1>Nyatinte Connect</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid h-full w-full grid-rows-2">
+	<section class="w-full p-10 text-center">
+		<div class="grid place-content-center">
+			<Avatar src="/nyatinte.png" size="xl" />
+		</div>
+		<h1 class="h1">Nyatinte</h1>
+	</section>
+	<div class="flex w-full flex-wrap justify-center gap-4 p-4">
+		{#each linkCards as card (card.href)}
+			<LinkCard {...card} />
+		{/each}
+	</div>
+</div>
