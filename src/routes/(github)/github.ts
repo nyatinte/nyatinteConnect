7d802c140057d0bc5946a8bc338cb3f5303a3d24
nyatinte <takumi.062757@gmail.com>
2023-11-29 23:10:25 +0900

    ✨各種リンクをstring→URLに変更

--- src/routes/(github)/github.ts
+++ src/routes/(github)/github.ts
@@ -1,4 +1,4 @@
 const GITHUB_URL = 'https://github.com';
 const USER_ID = 'nyatinte';
 
-export const NYATINTE_GITHUB_URL = `${GITHUB_URL}/${USER_ID}`;
+export const NYATINTE_GITHUB_URL = new URL(`${GITHUB_URL}/${encodeURIComponent(USER_ID)}`);
